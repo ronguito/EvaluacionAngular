a3d99f25254c8f455b48b150acbd312e899de63f
Raul Ramos <r.ramosg@uniandes.edu.co>
2024-11-02 13:39:22 -0500

    Creado servicio para consulta de datos url definida

--- src/app/vehiculos/vehiculos.component.ts
+++ src/app/vehiculos/vehiculos.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { ServidorService } from './servidor.service';
+import { Vehiculo } from './Vehiculo';
 
 @Component({
   selector: 'app-vehiculos',
   templateUrl: './vehiculos.component.html',
   styleUrls: ['./vehiculos.component.css']
 })
+
 export class VehiculosComponent implements OnInit {
 
-  constructor() { }
+  vehiculos: Array<Vehiculo> = []
+
+  constructor(private server:ServidorService) { }
 
   ngOnInit() {
+    this.getVehiculos()
+  }
+
+  getVehiculos(){
+    this.server.getVehiculos().subscribe(vehiculos => { 
+      this.vehiculos = vehiculos;   
+    });
   }
 
 }

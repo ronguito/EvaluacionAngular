931997f48839bf2d6eef0386151799e99fe82a70
Raul Ramos <r.ramosg@uniandes.edu.co>
2024-11-02 12:28:40 -0500

    Creado modulo vehiculos y clase Vehiculo

---
+++ src/app/vehiculos/Vehiculo.ts
@@ -0,0 +1,14 @@
+export class Vehiculo {
+    id: number = 1;
+    marca: string = "Renault";
+    linea: string = "Kangoo";
+    referencia: string = "VU Express";
+    modelo : number = 2017;
+    kilometraje: number = 93272;
+    color: string =  "Blanco";
+    imagen : string = "https://cdn.group.renault.com/ren/co/vehicles/kangoo/home/renault-kangoo-exterior.jpg";
+
+    constructor(){
+        
+    }
+}
